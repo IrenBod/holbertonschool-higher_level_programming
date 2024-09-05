a794ca4a3c001a3a840c869981fa1cb5ae0bdbf3
IrenBod <irinamora33@gmail.com>
2024-09-05 15:11:29 +0200

    add file 8-uppercase

---
+++ python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+def uppercase(str):
+
+    upp = ""
+    for i in str:
+        if 'a' <= i <= 'z':
+            upp += chr(ord(i) - 32)
+        else:
+            upp += (i)
+    print(upp)
