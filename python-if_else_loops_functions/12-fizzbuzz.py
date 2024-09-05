d4a6c88040b0608d215b2e62643a1626c66d85fb
IrenBod <irinamora33@gmail.com>
2024-09-05 18:36:21 +0200

    add 12-main 12-fizzbuzz

---
+++ python-if_else_loops_functions/12-fizzbuzz.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+for i in range(1, 101):
+    if i % 3 == 0 and i % 5 == 0:
+        print("FizzBuzz", end=" ")
+    elif i % 3 == 0:
+        print("Fizz", end=" ")
+    elif i % 5 == 0:
+        if i == 100:
+            print("Buzz")
+        else:
+            print("Buzz", end=" ")
+    else:
+        print("{}".format(i), end=" ")
