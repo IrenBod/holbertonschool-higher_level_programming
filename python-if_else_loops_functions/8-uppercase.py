ff02bda306467701245beb4778ca4a0fd6d8e830
IrenBod <irinamora33@gmail.com>
2024-09-05 15:19:02 +0200

    8-uppercase.py

--- python-if_else_loops_functions/8-uppercase.py
+++ python-if_else_loops_functions/8-uppercase.py
@@ -7,5 +7,8 @@ def uppercase(str):
         if 'a' <= i <= 'z':
             upp += chr(ord(i) - 32)
         else:
-            upp += (i)
-    print(upp)
+            upp += i
+    print("{}".format(upp))
+
+uppercase("best")
+uppercase("Best School 98 Battery street")
