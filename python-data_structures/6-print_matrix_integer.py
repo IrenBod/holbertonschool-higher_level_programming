fae02f38354176ffed945c963875c00ef4e00876
IrenBod <irinamora33@gmail.com>
2024-09-09 18:28:27 +0200

    add - if len(row) == 0:

--- python-data_structures/6-print_matrix_integer.py
+++ python-data_structures/6-print_matrix_integer.py
@@ -2,6 +2,8 @@
 
 def print_matrix_integer(matrix=[[]]):
     for row in matrix:
+        if len(row) == 0:
+            continue
         for i, element in enumerate(row):
             if i < len(row) - 1:
                 print("{:d}".format(element), end=" ")
