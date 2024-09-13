b131346d4904b36e073e82085a0e2a02cdfa4de7
IrenBod <irinamora33@gmail.com>
2024-09-13 13:01:29 +0200

    3-main 3-safe_print_division

---
+++ python-exceptions/3-safe_print_division.py
@@ -0,0 +1,9 @@
+def safe_print_division(a, b):
+
+    try:
+        result = a/b
+    except (TypeError, ZeroDivisionError):
+        result = None
+    finally:
+        print("Inside resulte: {}".format(result))
+        return result
