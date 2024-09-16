306a3694f26920ab95212f565a82dca0d72fc951
IrenBod <irinamora33@gmail.com>
2024-09-16 16:37:45 +0200

    docstring

--- python-classes/1-square.py
+++ python-classes/1-square.py
@@ -6,5 +6,11 @@ class Square:
     """
     A class that defines a square by its size.
     """
-    def __init__(self, size):
-        self.__size = size
+    def __init__(self, __size):
+        """
+    Initializes the square with a given size.
+    
+    Args:
+        size (int): The size of the square to initialize.
+    """
+        self.__size = __size
