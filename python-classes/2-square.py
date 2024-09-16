8156c2ac29fe5f15ded3e9884ea0d2efd94781cb
IrenBod <irinamora33@gmail.com>
2024-09-16 18:21:20 +0200

    1

--- python-classes/2-square.py
+++ python-classes/2-square.py
@@ -19,4 +19,5 @@ class Square:
             raise TypeError("size must be an integer")
         if size < 0:
             raise ValueError("size must be >=0")
+        # Private attribute
         self.__size = size
