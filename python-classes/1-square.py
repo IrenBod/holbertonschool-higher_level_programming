3a73e10871420eb7e2cbf53957f2a154d797172c
IrenBod <irinamora33@gmail.com>
2024-09-16 16:24:03 +0200

    1-main 1-square

---
+++ python-classes/1-square.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""
+Module defining the Square class.
+"""
+class Square:
+    """
+    A class that defines a square by its size.
+    """
+    def __init__(self, __size):
+        self.__size = __size
