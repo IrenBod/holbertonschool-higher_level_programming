61f088531ea38a0f2c7b7ed7ca0e3f854f2b5e9f
IrenBod <irinamora33@gmail.com>
2024-09-30 13:53:50 +0200

    commentaires

--- python-input_output/2-append_write.py
+++ python-input_output/2-append_write.py
@@ -1,4 +1,12 @@
 #!/usr/bin/python3
+"""
+Appends text to a file.
+"""
+
+
 def append_write(filename="", text=""):
+    """Appends text to filename and returns
+    the number of characters.
+    """
     with open(filename, 'a', encoding="utf-8") as f:
         return f.write(text)
