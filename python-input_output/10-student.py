ad1011cd6cc44cec93eacb4a1c9ff28551a8d6d8
IrenBod <irinamora33@gmail.com>
2024-10-02 12:25:18 +0200

    1

--- python-input_output/10-student.py
+++ python-input_output/10-student.py
@@ -1,4 +1,10 @@
 #!/usr/bin/python3
+"""
+This module defines a Student class and its methods to retrieve
+a dictionary representation of the instance.
+"""
+
+
 class Student:
     def __init__(self, first_name, last_name, age):
         """
@@ -11,10 +17,12 @@ class Student:
     def to_json(self, attrs=None):
         """
         Retrieves a dictionary representation of the Student instance.
-        If attrs is a list of strings, only the attributes with those names will be included.
+        If attrs is a list of strings, only the attributes
+        with those names will be included.
         Otherwise, all attributes will be included.
         """
         if attrs is None:
             return self.__dict__
-        return {key: value for key, value in self.__dict__.items() if key in attrs}
-
+        return {key: value
+                for key, value in self.__dict__.items()
+                if key in attrs}
