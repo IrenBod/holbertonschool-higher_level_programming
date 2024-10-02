17d032916756ac01960bc0f66a0036e4072cd0c8
IrenBod <irinamora33@gmail.com>
2024-10-02 12:18:06 +0200

    10-student.py, 10-main.py

---
+++ python-input_output/10-student.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+class Student:
+    def __init__(self, first_name, last_name, age):
+        """
+        Initialize a new student instance.
+        """
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """
+        Retrieves a dictionary representation of the Student instance.
+        If attrs is a list of strings, only the attributes with those names will be included.
+        Otherwise, all attributes will be included.
+        """
+        if attrs is None:
+            return self.__dict__
+        return {key: value for key, value in self.__dict__.items() if key in attrs}
+
