823871540ac807fa0ac4d25f6412d92253c58282
IrenBod <irinamora33@gmail.com>
2024-10-09 09:51:10 +0200

    task_02_requests.py

--- restful-api/task_02_requests.py
+++ restful-api/task_02_requests.py
@@ -3,14 +3,16 @@ import requests
 import json
 import csv
 
+
 def fetch_and_print_posts():
     response = requests.get("https://jsonplaceholder.typicode.com/posts")
     if response.status_code == 200:
+        print(f"Status Code: {response.status_code}")
         posts = response.json()
         for post in posts:
             print(post["title"])
-        else:
-            print("Error print")
+    else:
+        print("Error print")
 
 
 def fetch_and_save_posts():
@@ -19,18 +21,19 @@ def fetch_and_save_posts():
         posts = response.json()
         structured_posts = []
         for post in posts:
-            structured_posts.append = ({
+            structured_posts.append({
                 'id': post['id'],
                 'title': post['title'],
                 'body': post['body']
             })
-        
-        with open('posts.csv','w') as csvfile:
-            writer = csv.DictWriter(csvfile, fieldnames=['id', 'title', 'body'])
+
+        with open('posts.csv', 'w', newline='') as csvfile:
+            writer = csv.DictWriter(
+                csvfile,
+                fieldnames=['id', 'title', 'body']
+                )
             writer.writeheader()
             writer.writerows(structured_posts)
-        
-        print(f"Status Code: {response.status_code}")
-    
+
     else:
         print("Error save")
