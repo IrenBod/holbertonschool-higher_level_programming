3dd273eb9a0e47a4a75668666de14198a1bc9077
IrenBod <irinamora33@gmail.com>
2024-10-24 21:42:56 +0200

    1

--- SQL_introduction/16-no_link.sql
+++ SQL_introduction/16-no_link.sql
@@ -1,15 +1,6 @@
 -- a script that lists all records of the table second_table
 -- of the database hbtn_0c_0 in your MySQL server.
-UPDATE second_table
-SET name = NULL
-WHERE id = 4;
-
-INSERT INTO second_table (id, name, score)
-VALUES
-(5, "Aria", 18),
-(6, "Aria", 12);
-
-SELECT score, name FROM second_table
+SELECT score, name
+FROM second_table
 WHERE name IS NOT NULL
-GROUP BY score, name
-ORDER BY score DESC, name;
+ORDER BY score DESC;
