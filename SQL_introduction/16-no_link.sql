37f57a36d41eb270e61772cbaefbf9360aaea341
IrenBod <irinamora33@gmail.com>
2024-10-24 21:19:59 +0200

    1

--- SQL_introduction/16-no_link.sql
+++ SQL_introduction/16-no_link.sql
@@ -1,4 +1,4 @@
-UPDATE hbtn_0c_0.second_table
+UPDATE second_table
 SET name = NULL
 WHERE id = 4;
 
@@ -7,7 +7,7 @@ VALUES
 (5, "Aria", 18),
 (6, "Aria", 12);
 
-SELECT score, name FROM hbtn_0c_0.second_table
+SELECT score, name FROM second_table
 WHERE name IS NOT NULL
 GROUP BY score, name
 ORDER BY score DESC, name;
--- SQL_introduction/4-first_table.sql
+++ SQL_introduction/4-first_table.sql
@@ -1,5 +1,5 @@
-/*This script creates a table named first_table in
-the current database if it doesn't already exist.*/
+-- This script creates a table named first_table in
+-- the current database if it doesn't already exist.
 CREATE TABLE IF NOT EXISTS first_table
 (
     id INT,
