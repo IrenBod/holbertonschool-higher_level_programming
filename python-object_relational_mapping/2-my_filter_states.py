a8ec17e82f4a347268cb02d50ac5b184168ed22f
IrenBod <irinamora33@gmail.com>
2024-11-01 15:06:17 +0100

    1

--- python-object_relational_mapping/2-my_filter_states.py
+++ python-object_relational_mapping/2-my_filter_states.py
@@ -23,7 +23,7 @@ if __name__ == "__main__":
     cursor = db.cursor()
 
     # Execute SQL query to find states matching the specified name
-    query = "SELECT * FROM states WHERE name = '{}'\
+    query = "SELECT * FROM states WHERE BINARY name = '{}'\
     ORDER BY id ASC".format(state_name_searched)
     cursor.execute(query)
 
