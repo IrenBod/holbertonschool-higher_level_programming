60fa5225f35c176d3c86aa351074a6f979873203
IrenBod <irinamora33@gmail.com>
2024-11-02 19:17:29 +0100

    1

--- python-object_relational_mapping/3-my_safe_filter_states.py
+++ python-object_relational_mapping/3-my_safe_filter_states.py
@@ -1,38 +1,40 @@
-#!/usr/bin/env python3
-# This script connects to a MySQL database and
-# safely retrieves entries from the states table.
-# Usage: ./3-my_safe_filter_states.py <username>
-# <password> <database> <state_name>
+#!/usr/bin/python3
+"""This module explains SQL injection."""
 
 import MySQLdb
 import sys
 
 if __name__ == "__main__":
-    # Get command-line arguments
+    # Get the commands-line
     mysql_username = sys.argv[1]
     mysql_password = sys.argv[2]
     database_name = sys.argv[3]
     state_name_searched = sys.argv[4]
 
-    # Connect to the MySQL database on localhost at port 3306
-    db = MySQLdb.connect(host="localhost", 
-                         port=3306, user=mysql_username,
-                         passwd=mysql_password, db=database_name)
+    # Connect to database
+    connexion = MySQLdb.connect(
+        host="localhost",
+        user=mysql_username,
+        passwd=mysql_password,
+        db=database_name,
+        port=3306,
+    )
 
-    # Create a cursor object to execute SQL queries
-    cursor = db.cursor()
+    # Create a cursor
+    cursor = connexion.cursor()
 
-    # Define the query with a parameterized placeholder to prevent SQL injection
-    query = "SELECT * FROM states WHERE name = %s ORDER BY id ASC"
-    cursor.execute(query, (state_name_searched,))
+    # Execute safe SQL query
+    sql = "SELECT * FROM states WHERE states.name = %s ORDER BY states.id ASC"
+    # Place state_name_search in an immutable tuple
+    cursor.execute(sql, (state_name_searched,))
 
-    # Fetch all rows from the executed query
+    # Get the results
     rows = cursor.fetchall()
 
-    # Display each row as a tuple
+    # Print results
     for row in rows:
         print(row)
 
-    # Close cursor and database connection
+    # Clean up
     cursor.close()
-    db.close()
+    connexion.close()
