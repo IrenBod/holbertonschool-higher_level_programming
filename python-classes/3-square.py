08f8a9d30cb06589351235621ca1dcd68c4f668b
IrenBod <irinamora33@gmail.com>
2024-12-12 15:03:15 +0100

    1

--- python-classes/3-square.py
+++ python-classes/3-square.py
@@ -6,16 +6,18 @@ of the square and calculating its area."""
 
 
 class Square():
+    """ Class that define a square """
     def __init__(self, size=0):
         """Initialize the square with an optional size parameter
          (default is 0).
         Validate that the size is an integer and greater than
          or equal to 0."""
-        self.__size = size
+
         if not isinstance(size, int):
             raise TypeError("size must be an integer")
         if size < 0:
             raise ValueError("size must be >= 0")
+        self.__size = size
 
     def area(self):
         """Calculate and return the area of the square.
